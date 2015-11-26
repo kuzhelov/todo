b582f614080ad19b650836dc56334a0cd81b06bf
kuzhelov <deckster@mail.ru>
2015-11-27 01:24:14 +0300

    introduce initial design for mobile screens

---
+++ resources/js/script.js
@@ -0,0 +1,18 @@
+function isEmptyString(str) {
+    return !str || str.length === 0;
+}
+
+angular
+    .module("todoApp", [])
+    .controller("mainController", function($scope) {})
+    .controller("newTaskController", function($scope) {
+        
+        $scope.newTask = {
+            text: "",
+            isReadyToBeAdded: function() {
+                console.log('its text is ' + this.text);
+                return !isEmptyString(this.text);
+            },
+            isFocused: false
+        };
+    });
